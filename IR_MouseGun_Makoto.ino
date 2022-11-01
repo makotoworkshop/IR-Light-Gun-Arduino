45b1de75044fbbe648ad7db31e8e6b9e8270ace2
makotoworkshop <makoto@debianbox.makotoworkshop.org>
2022-11-01 16:25:32 +0100

    Update IR_MouseGun_Makoto.ino

--- IR_MouseGun_Makoto.ino
+++ IR_MouseGun_Makoto.ino
@@ -1,92 +1,74 @@
 /* Programme "Wii Camera to Pijama" v1.0
  * Par Aganyte, April 2020
+ * Modifs Makoto Novembre 2022 :
+ *  ok - Ajout du HID Joystick simultané (Stick XY et boutons)
+ *  ok - Mode Souris : Reload activé si la souris sort de l'écran.
+ *  - Switch de sélection pour désactiver mode Souris. (bien utile parfois…)
  */
- // Modifs Makoto en cours…
 
 #include <math.h>
 #include "WiiLib.h"
 #include "MouseAbs.h"
-#include <Joystick.h>
+#include <Joystick.h> // Librairie Joystick by Matthew Heironimus V2.0.7
 
-//#——JOYSTICK——#
+// Définition du Joystick
 Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,JOYSTICK_TYPE_JOYSTICK,
-  1, 0,                  // Button Count, Hat Switch Count
-  true, true, false,     // X and Y, but no Z Axis
-  false, false, false,   // No Rx, Ry, or Rz
-  false, false,          // No rudder or throttle
-  false, false, false);  // No accelerator, brake, or steering
-//#——JOYSTICK——#
+  3, 0,                           // Button Count, Hat Switch Count
+  true, true, false,              // X and Y, but no Z Axis
+  false, false, false,            // No Rx, Ry, or Rz
+  false, false,                   // No rudder or throttle
+  false, false, false);           // No accelerator, brake, or steering
+const int pinToButtonMap = 4;     // Constant that maps the physical pin to the first joystick button.
+int lastButtonState[3] = {0,0,0}; // Last state of the 3 buttons
+int lastReloadState = 0;          // Spécial pour Reload qui n'est pas câblé
 
-
-/* definition des equivalences */
+// Définition et réglages Camera IR
 #define	Ratio	 			        1 			      // Ratio de l'ecran 1 = 4/3 et 0 = 16/9
 #define Xcenter				      fullscale/2		// Position du centre de la camera sur X
 #define Ycenter				      fullscale/2		// Position du centre de la camera sur Y
 #define fullscale	  		    255				    // Valeur Max (Resolution de la camera moins 1)
 #define Resolution_Pn  	    255				    // Valeur Max (Resolution des potentiometres numerique moins 1)
+unsigned int Screen_wide = 55;            // largeur de l'écran en centimetres
+signed int Y_Correction =  28;            // Décallage en Hauteur par rapport à la barre IR
+signed int X_Correction = 0;              // Décallage en Largeur (ou déplacer un tipeu la barre ;))
+unsigned int Wiibar_size = 20;            // Distance entre les 2 emetteurs Infra-Rouges
 
-/* Réglages */
-unsigned int Screen_wide = 55; // largeur de l'écran en centimetres
-signed int Y_Correction =  28;
-signed int X_Correction = 0;
-unsigned int Wiibar_size = 20; // Taille de la Wii bar en centimetres (ou distance entre les 2 emetteurs I.R)
+unsigned int selecteur = 0;
+unsigned int ModeReload = 0;
 
 void setup()
 {
 //  Serial.begin(115200); // Debug Mode (9600 for processing)
   Wire.begin();
 
-// Décommenter TWBR si la cam ne fonctionne pas
-//  TWBR = 1 ; // max bit rate possible 800khz // IR_cam support the Fast Plus Mode I2C  
+  // Initialisation Camera IR (Décommenter TWBR si la cam ne fonctionne pas)
+  // TWBR = 1 ; // max bit rate possible 800khz // IR_cam support the Fast Plus Mode I2C  
   // To change the bit rate use TWBR = ((F_CPU / F_I2C) - 16) / 2
   // For example 400khz (Fast Mode) with 16Mhz oscillator => TWBR = ((16000000L / 400000L) - 16) / 2
-
-  // IR sensor initialize
   Write_2bytes(0x30,0x08); delay(10);  // begin config
   Write_Nbytes(0x00,setting1,9); delay(10); // sensiblity 1
   Write_Nbytes(0x1A,setting2,2); delay(10); // sensiblity 2
   Write_2bytes(0x33,0x03); delay(10); // data mode extended
- 
-  Mouse.init(); // Activer le mode souris
 
-  /* Activation des entrées */
-  pinMode(4,INPUT_PULLUP);  // Click Droit (Reload)
+  // Bouton de Souris
+  pinMode(4,INPUT_PULLUP);  // Click Droit (Reload) non cablé, actif par logiciel Souris ou Joystick
   pinMode(5,INPUT_PULLUP);  // Click Gauche (Tir)
-  pinMode(6,INPUT_PULLUP);  // Click Milieu (Start)
+  Mouse.init(); // Activer le mode Souris
 
-//#——JOYSTICK——# 
-  // Initialize Button Pins
+  // Boutons de Joystick
   pinMode(6, INPUT_PULLUP);
-
-//  Joystick.setXAxisRange(-127, 127);
-//  Joystick.setYAxisRange(-127, 127);
-  
   Joystick.setXAxisRange(0, 255);
   Joystick.setYAxisRange(0, 255);
-  pinMode(A0, INPUT_PULLUP);
-  pinMode(A1, INPUT_PULLUP);
-  // Initialize Joystick Library
   Joystick.begin();
-//#——JOYSTICK——#
-  
+
+  // Switch selecteurs
+  pinMode(8, INPUT_PULLUP); // Mode reload
+  pinMode(9, INPUT_PULLUP); // (Actif par défaut si non câblé) Désactivation Mode Souris (le HID est présent, mais les data ne sont plus envoyées à la souris
 }
 
-//#——JOYSTICK——#
-// Constant that maps the phyical pin to the joystick button.
-const int pinToButtonMap = 6;
 
-// Last state of the button
-int lastButtonState = 0;
-//#——JOYSTICK——#
-
-
-/* Programme principal */
 void loop()
 {
-
-
-
-  
   /* Variables */
   unsigned char Counter = 0;
   unsigned int Ymoy;
@@ -99,7 +81,7 @@ void loop()
 	/* Boucle infinie du mode run */
 	for(;;)
 	{
-		unsigned long RefreshTime = millis();
+    unsigned long RefreshTime = millis();
 		
 		/* Capturer 2 blobs */
 		ReadCamera();
@@ -150,13 +132,10 @@ void loop()
 		Yc -= ( Y_Correction * Widesize ) / Screen_wide; 
 		Xc -= ( X_Correction * Widesize ) / Screen_wide;
 
-
     Serial.print("Bouton Clic Droit : ");
     Serial.println(digitalRead(4));
     Serial.print("Bouton Clic Gauche : ");  
     Serial.println(digitalRead(5));
-    Serial.print("Bouton Clic Milieu : ");  
-    Serial.println(digitalRead(6));
 
     /* RAFRAICHIR LES BOUTONS */
     // Gestion du clic gauche -> Tir
@@ -165,20 +144,6 @@ void loop()
     else
       Mouse.release(MOUSE_LEFT);
     
-    // Gestion du clic droit désactivé pour mode RELOAD automatique cf. en bas
-//    if(digitalRead(4)==LOW) -> Reload
-//      Mouse.press(MOUSE_RIGHT);
-//    else
-//      Mouse.release(MOUSE_RIGHT);
-    
-    // Gestion du clic central  -> Start
-//    if(digitalRead(6)==LOW)
-//      Mouse.press(MOUSE_MIDDLE);
-//    else
-//      Mouse.release(MOUSE_MIDDLE);
-
-
-
 		/* Calcul de XC et YC et conversion 8 bits */
 		Yc = (Yc * Resolution_Pn ) / Highsize;
 		Xc = (Xc * Resolution_Pn ) / Widesize;
@@ -204,58 +169,75 @@ void loop()
 		}
 		else
 		{
-			Mouse.Actual_X = map((int)Xc,0,255,32767,0);
-			Mouse.Actual_Y = map((int)Yc,0,255,0,32767);
+			Mouse.Actual_X = map((int)Xc, 0, 255, 32767, 0);
+			Mouse.Actual_Y = map((int)Yc, 0, 255, 0, 32767);
 		} 
-    Serial.print("Actual_X : "); 
+    Serial.print("Souris Actual_X : "); 
 		Serial.print((int)Mouse.Actual_X);
 		Serial.print(",  Actual_Y : ");
 		Serial.println((int)Mouse.Actual_Y);
 
-
-    /* Mode RELOAD automatique si le curseur sort de l'écran */
-    if ( (Mouse.Actual_X == 1) & (Mouse.Actual_Y == 1) )
+    /* Mode RELOAD automatique si le curseur sort de l'écran, géré par logiciel */
+    ModeReload = digitalRead(8);
+    if (ModeReload == 0)  // Si le mode Reload est activé avec le switch Reload
     {
-      Serial.println("Reload !!! Clic Droit");   
-      Mouse.press(MOUSE_RIGHT);
+      if ( (Mouse.Actual_X == 1) & (Mouse.Actual_Y == 1) )  // si on sort de l'écran
+      {
+        Serial.println("Reload !!! Clic Droit");   
+        Mouse.press(MOUSE_RIGHT);
+        Joystick.setButton(0, HIGH);  // Le reload est sur l'entrée 4, première décrite, donc « button 0 »
+        lastReloadState = HIGH;
+      }
+      else
+      {
+        Mouse.release(MOUSE_RIGHT);
+        Joystick.setButton(0, LOW);
+        lastReloadState = LOW; 
+      }
     }
     else
     {
-//      Serial.println("Load !!! ");   
+      Serial.println("Mode Reload désactivé");
       Mouse.release(MOUSE_RIGHT);
+      Joystick.setButton(0, LOW);
+      lastReloadState = LOW; 
     }
 
     Serial.println(""); // Paragraphe
 
-//#——JOYSTICK——#
-  int xAxis = analogRead(A0);
-  int yAxis = analogRead(A1);
-  
-  Serial.print("A0 = ");
-  Serial.print(analogRead(A0));
-  Serial.print("  A1 = ");
-  Serial.println(analogRead(A1));
+// Bloc JOYSTICK à situer impérativement ici
+  // Valeurs du Stick envoyées au HID Device
+    int xAxis = map(Xc, 0, 255, 255, 0);  // pour inverser l'axe X
+    int yAxis = Yc;
 
-//byte red  = map(xAxis, 0, 255, -127, 127);
-//byte blue = map(yAxis, 0, 255, -127, 127);
+    Serial.print("Joystick xAxis = ");
+    Serial.print(xAxis);
+    Serial.print(",  yAxis = ");
+    Serial.println(yAxis);
   
-  Joystick.setXAxis(xAxis);
-  Joystick.setYAxis(yAxis);
+    Joystick.setXAxis(xAxis);
+    Joystick.setYAxis(yAxis);
 
-  // Read pin values
-    int currentButtonState = !digitalRead(pinToButtonMap);
-    if (currentButtonState != lastButtonState)
+  // Valeurs des boutons envoyées au HID Device
+    for (int index = 0; index < 3; index++)
     {
-      Joystick.setButton(0, currentButtonState);
-      lastButtonState = currentButtonState;
+    int currentButtonState = !digitalRead(index + pinToButtonMap);
+    if (currentButtonState != lastButtonState[index])
+      {
+        Joystick.setButton(index, currentButtonState);
+        lastButtonState[index] = currentButtonState;
+      }
     }
-
-//  delay(50);
-//#——JOYSTICK——#
-
-
+    Serial.print("Bouton Joystick : ");
+    Serial.println(digitalRead(6));
+// Bloc JOYSTICK Fin
     
 		while( (millis() - RefreshTime) < 20); // 20ms => 50 Hertz
-		Mouse.report(); // mettre à jour le curseur et les boutons
+
+    selecteur = digitalRead(9);
+    if (selecteur == 1)
+  		Mouse.report(); // mettre à jour le curseur et les boutons
+    else
+      Serial.println("Mode Souris désactivé");
 	}
 }
